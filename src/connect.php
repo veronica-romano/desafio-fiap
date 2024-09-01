48206b47ab6793b019d74e48e5f7fa88e517ade5
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2024-09-01 16:29:04 -0300

    alterando configuração de banco de dados

--- src/connect.php
+++ src/connect.php
@@ -1,10 +1,12 @@
 <?php
 /*Script de conexão ao servidor banco de dados */
+$ini = parse_ini_file("php.ini");
 
-$servidor = "localhost";
-$usuario = "ruth";
-$senha = "senha-muito-longa";
-$banco = "desafio_fiap";
+$servidor = $ini["servidor"];
+$usuario = $ini["usuario"];
+$senha = $ini["senha"];
+$banco = $ini["banco"];
+
 try{
 //Criando a conexão com o MySQL (API/ Driver de conexão)
     $conexao = new PDO("mysql:host=$servidor; dbname=$banco; charset=utf8", $usuario, $senha);
